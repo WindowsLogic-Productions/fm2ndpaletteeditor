12825c72cc496a570157c7a1a177c13d67a451e1
Xem85 <290998366+xem85@users.noreply.github.com>
2021-06-24 09:56:38 +0200

    first commit

---
+++ Fm2ndPaletteEditor/Form1.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace Fm2ndPaletteEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbBitmap = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbRed = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbGreen = new System.Windows.Forms.TrackBar();
+            this.tbBlue = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbH = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbL = new System.Windows.Forms.TrackBar();
+            this.tbS = new System.Windows.Forms.TrackBar();
+            this.lblImageHint = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBitmap)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbBitmap
+            // 
+            this.pbBitmap.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pbBitmap.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbBitmap.Location = new System.Drawing.Point(12, 12);
+            this.pbBitmap.Name = "pbBitmap";
+            this.pbBitmap.Size = new System.Drawing.Size(340, 351);
+            this.pbBitmap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbBitmap.TabIndex = 0;
+            this.pbBitmap.TabStop = false;
+            this.pbBitmap.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 16;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(682, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 16;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(368, 338);
+            this.tableLayoutPanel1.TabIndex = 1;
+            this.tableLayoutPanel1.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.tableLayoutPanel1_CellPaint);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(13, 716);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(339, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbRed
+            // 
+            this.tbRed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbRed.LargeChange = 32;
+            this.tbRed.Location = new System.Drawing.Point(32, 410);
+            this.tbRed.Maximum = 256;
+            this.tbRed.Minimum = -256;
+            this.tbRed.Name = "tbRed";
+            this.tbRed.Size = new System.Drawing.Size(319, 45);
+            this.tbRed.SmallChange = 8;
+            this.tbRed.TabIndex = 3;
+            this.tbRed.TickFrequency = 8;
+            this.tbRed.Scroll += new System.EventHandler(this.tb_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 410);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "R";
+            // 
+            // tbGreen
+            // 
+            this.tbGreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbGreen.LargeChange = 32;
+            this.tbGreen.Location = new System.Drawing.Point(32, 461);
+            this.tbGreen.Maximum = 256;
+            this.tbGreen.Minimum = -256;
+            this.tbGreen.Name = "tbGreen";
+            this.tbGreen.Size = new System.Drawing.Size(319, 45);
+            this.tbGreen.SmallChange = 8;
+            this.tbGreen.TabIndex = 3;
+            this.tbGreen.TickFrequency = 8;
+            this.tbGreen.Scroll += new System.EventHandler(this.tb_Scroll);
+            // 
+            // tbBlue
+            // 
+            this.tbBlue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbBlue.LargeChange = 32;
+            this.tbBlue.Location = new System.Drawing.Point(33, 512);
+            this.tbBlue.Maximum = 256;
+            this.tbBlue.Minimum = -256;
+            this.tbBlue.Name = "tbBlue";
+            this.tbBlue.Size = new System.Drawing.Size(318, 45);
+            this.tbBlue.SmallChange = 8;
+            this.tbBlue.TabIndex = 3;
+            this.tbBlue.TickFrequency = 8;
+            this.tbBlue.Scroll += new System.EventHandler(this.tb_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "G";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 512);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "B";
+            // 
+            // tbH
+            // 
+            this.tbH.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbH.LargeChange = 32;
+            this.tbH.Location = new System.Drawing.Point(33, 563);
+            this.tbH.Maximum = 256;
+            this.tbH.Minimum = -256;
+            this.tbH.Name = "tbH";
+            this.tbH.Size = new System.Drawing.Size(319, 45);
+            this.tbH.SmallChange = 8;
+            this.tbH.TabIndex = 3;
+            this.tbH.TickFrequency = 8;
+            this.tbH.Scroll += new System.EventHandler(this.tb_Scroll);
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 563);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 15);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "H";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 614);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "L";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 665);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "S";
+            // 
+            // tbL
+            // 
+            this.tbL.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbL.LargeChange = 32;
+            this.tbL.Location = new System.Drawing.Point(33, 614);
+            this.tbL.Maximum = 256;
+            this.tbL.Minimum = -256;
+            this.tbL.Name = "tbL";
+            this.tbL.Size = new System.Drawing.Size(319, 45);
+            this.tbL.SmallChange = 8;
+            this.tbL.TabIndex = 3;
+            this.tbL.TickFrequency = 8;
+            this.tbL.Scroll += new System.EventHandler(this.tb_Scroll);
+            // 
+            // tbS
+            // 
+            this.tbS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.tbS.LargeChange = 32;
+            this.tbS.Location = new System.Drawing.Point(34, 665);
+            this.tbS.Maximum = 256;
+            this.tbS.Minimum = -256;
+            this.tbS.Name = "tbS";
+            this.tbS.Size = new System.Drawing.Size(318, 45);
+            this.tbS.SmallChange = 8;
+            this.tbS.TabIndex = 3;
+            this.tbS.TickFrequency = 8;
+            this.tbS.Scroll += new System.EventHandler(this.tb_Scroll);
+            // 
+            // lblImageHint
+            // 
+            this.lblImageHint.AutoSize = true;
+            this.lblImageHint.Location = new System.Drawing.Point(98, 174);
+            this.lblImageHint.Name = "lblImageHint";
+            this.lblImageHint.Size = new System.Drawing.Size(150, 15);
+            this.lblImageHint.TabIndex = 5;
+            this.lblImageHint.Text = "Click to load sample image";
+            this.lblImageHint.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1062, 751);
+            this.Controls.Add(this.tbS);
+            this.Controls.Add(this.tbBlue);
+            this.Controls.Add(this.tbL);
+            this.Controls.Add(this.tbGreen);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbH);
+            this.Controls.Add(this.tbRed);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.lblImageHint);
+            this.Controls.Add(this.pbBitmap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbBitmap)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.PictureBox pbBitmap;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar tbRed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbGreen;
+        private System.Windows.Forms.TrackBar tbBlue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TrackBar tbH;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar tbL;
+        private System.Windows.Forms.TrackBar tbS;
+        private System.Windows.Forms.Label lblImageHint;
+    }
+}
+
---
+++ Fm2ndPaletteEditor/Form1.cs
@@ -0,0 +1,225 @@
+﻿using ImageProcessor.Imaging.Quantizers;
+using Sc.Util.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fm2ndPaletteEditor
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap _bitmap { get; set; }
+        public Color[] _originalPalette { get; set; }
+        public Color[] _palette { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("shlwapi.dll")]
+        public static extern int ColorHLSToRGB(int H, int L, int S);
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new OpenFileDialog())
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        loadImage(dialog.FileName);
+
+                        lblImageHint.Visible = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error: {ex.Message}");
+                    }
+                }
+            }
+        }
+
+        private void loadImage(string filename)
+        {
+            var image = Image.FromFile(filename);
+            var bitmap = new Bitmap(image);
+            _bitmap = convertToIndexed(bitmap);
+            pbBitmap.Image = _bitmap;
+            var transpIdx = Array.FindIndex(_bitmap.Palette.Entries, x => x.ToArgb() == Color.Black.ToArgb());
+            _bitmap.Palette.Entries[transpIdx] = Color.FromArgb(255, 0, 0, 0);
+            _bitmap.Palette = _bitmap.Palette;
+
+            _originalPalette = (Color[])_bitmap.Palette.Entries.Clone();
+            applyTransformation();
+        }
+
+        private void applyTransformation()
+        {
+            if (_bitmap != null)
+            {
+                ColorPalette pal = _bitmap.Palette;
+                for (int i = 0; i < _originalPalette.Length; i++)
+                {
+                    pal.Entries[i] = transformColor(_originalPalette[i]);
+
+                }
+
+                _bitmap.Palette = pal;
+                _palette = _bitmap.Palette.Entries;
+                this.pbBitmap.Refresh();
+                tableLayoutPanel1.Refresh();
+            }
+        }
+
+        private Color transformColor(Color color)
+        {
+            var result = color;
+            if (color.ToArgb() != Color.Black.ToArgb())
+            {
+                result = Color.FromArgb(color.A,
+                minMax(color.R + tbRed.Value),
+                minMax(color.G + tbGreen.Value),
+                minMax(color.B + tbBlue.Value)
+            );
+                var hsl = SimpleColorTransforms.RgBtoHsl(result);
+                result = SimpleColorTransforms.HsLtoRgb(
+                       hsl[0] + tbH.Value,
+                       hsl[1] + ((float)tbS.Value / 255),
+                       hsl[2] + ((float)tbL.Value / 255)
+                    );
+
+                //var hsb = SimpleColorTransforms.RgBtoHsb(result);
+                //result = SimpleColorTransforms.HsBtoRgb(
+                //       hsb[0] + tbH.Value,
+                //       hsb[1] + ((float)tbS.Value / 255),
+                //       hsb[2] + ((float)tbL.Value / 255)
+                //    );
+
+
+                //var hls = ColorHLSToRGB(
+                //    minMax((int)Math.Round(result.GetHue() / 360 * 240) + tbH.Value, 240),
+                //    minMax((int)Math.Round(result.GetBrightness() * 240) + tbL.Value, 240),
+                //    minMax((int)Math.Round(result.GetSaturation() * 240) + tbS.Value, 240)
+                //);
+
+                //result = ColorTranslator.FromWin32(hls);
+            }
+            return result;
+        }
+
+        private int minMax(int v, int max = 255)
+        {
+            return Math.Max(Math.Min(v, max), 0);
+        }
+
+        private double minMax(double v, double max = 255)
+        {
+            return Math.Max(Math.Min(v, max), 0);
+        }
+
+        unsafe private Bitmap convertToIndexed(Bitmap bmp)
+        {
+            OctreeQuantizer quantizer = new OctreeQuantizer(255, 8);
+
+            using (Bitmap quantized = quantizer.Quantize(bmp))
+            {
+                return quantized.Clone(new Rectangle(0, 0, bmp.Width, bmp.Height), PixelFormat.Format8bppIndexed);
+            }
+        }
+
+        private void tableLayoutPanel1_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            var idx = (e.Row * 16) + e.Column;
+            if (_bitmap != null)
+            {
+                var color = idx < _palette.Count() ?
+                    _bitmap.Palette.Entries[idx] :
+                    Color.Transparent;
+                using (SolidBrush brush = new SolidBrush(color))
+                    e.Graphics.FillRectangle(brush, e.CellBounds);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var palette = new List<Color>();
+            var tableTxt = Clipboard.GetText();
+            using (var reader = new StringReader(tableTxt))
+            {
+                var idx = 0;
+                var buffer = new Span<char>(new char[12]);
+                var count = 0;
+                while ((count = reader.Read(buffer)) > 0)
+                {
+                    var colorTxt = new String(buffer);
+                    try
+                    {
+                        var color = parseFM2kColor(colorTxt);
+                        var newColor = transformColor(color);
+                        palette.Add(newColor);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error parsing color n.{idx} '{colorTxt}': {ex.Message}");
+                        return;
+                    }
+
+                    idx++;
+                }
+            }
+
+            using (var writer = new StringWriter())
+            {
+                foreach (var color in palette)
+                {
+                    var colorTxt = toFM2kColor(color);
+                    writer.Write(colorTxt);
+                }
+
+                Clipboard.SetText(writer.ToString());
+            }
+        }
+
+        private object toFM2kColor(Color color)
+        {
+            var r = (int)Math.Round((double)color.R / 8) * 8;
+            var g = (int)Math.Round((double)color.G / 8) * 8;
+            var b = (int)Math.Round((double)color.B / 8) * 8;
+            return $"{b.ToString("X2")} {g.ToString("X2")} {r.ToString("X2")} 01 ";
+        }
+
+
+        private Color parseFM2kColor(string colorTxt)
+        {
+            var rgba = colorTxt.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            if (rgba.Count() != 4)
+                throw new Exception("Wrong format");
+            if (rgba[3] != "01")
+                throw new Exception("No global palette");
+
+            var b = int.Parse(rgba[0], NumberStyles.HexNumber);
+            var g = int.Parse(rgba[1], NumberStyles.HexNumber);
+            var r = int.Parse(rgba[2], NumberStyles.HexNumber);
+
+            var color = Color.FromArgb(r, g, b);
+            return color;
+        }
+
+        int smallChangeValue = 8;
+        private void tb_Scroll(object sender, EventArgs e)
+        {
+            applyTransformation();
+        }
+    }
+}
