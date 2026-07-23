edfb8bf43feb415503cd7c6b7ce1f22f5704f25b
Alethila <admin@windowslogic.co.uk>
2026-07-23 11:41:52 +0100

    Add dependencies, rewrite gitignore.

---
+++ fm2ndpaletteeditor WinXP/fm2ndPaletteEditor/fm2ndPaletteEditor/Main.cs
@@ -0,0 +1,20 @@
+﻿using ColorMine.ColorSpaces;
+//using Fm2ndPaletteEditor.Service;
+using ImageProcessor.Imaging.Quantizers;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace fm2ndPaletteEditor
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ fm2ndpaletteeditor WinXP/fm2ndPaletteEditor/fm2ndPaletteEditor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+
+namespace fm2ndPaletteEditor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
